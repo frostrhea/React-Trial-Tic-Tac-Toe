9ee2716932402db46ea3d12efe9287385f500bcb
frostrhea <rheafrost865@gmail.com>
2023-01-31 22:28:13 +0800

    attempting to add img

---
+++ image.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Images(){
+    return(
+        <div>
+            <img src={'./public/Plain-Pale-Blue-Background11.jpg'} alt=""/>
+           
+        </div>
+    )
+}
+
+export default Images;
